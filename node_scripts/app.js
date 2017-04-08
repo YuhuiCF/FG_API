7f089f80b2293d0d273de64b97392f73b6795af8
Yuhui Xiong <Yuhui.Xiong@mcon-group.com>
2017-04-08 21:56:55 +0200

    add node version of fg-api-core

---
+++ node_scripts/app.js
@@ -0,0 +1,95 @@
+
+const fgApi = require('./fg-api-core');
+const newBids = require('./bids');
+const Promise = require('bluebird');
+
+const locationIds = [1038374, 1059199, 1072961, 1323754, 1063988];
+
+
+
+fgApi.configure('https://api-qa.fairgarage.de');
+
+fgApi
+  .login({
+    requestBody: {
+      username: 'admin@fairgarage.de',
+      password: 'abc123'
+    }
+  })
+  .then(() => {
+    let promises = [];
+    let currentBidsOfLocations = [];
+
+    locationIds.forEach((locationId) => {
+      const params = {
+        requestParams: {locationId: locationId}
+      };
+
+      promises.push(
+        fgApi.listBids(params).then((bids) => {
+          currentBidsOfLocations = currentBidsOfLocations.concat(bids);
+        })
+      );
+    });
+
+    return Promise.all(promises).then(() => {
+      let deleteBidPromises = Promise.resolve();
+
+      console.log('number of bids: ' + currentBidsOfLocations.length);
+      currentBidsOfLocations.forEach((bid) => {
+        //console.log(bid);
+        const params = {
+          pathParams: {bidKey: bid.key}
+        };
+
+        deleteBidPromises = deleteBidPromises.then(() => {
+          console.log('deleting bid ' + bid.key);
+          return fgApi.deleteBid(params);
+        });
+      });
+
+      return deleteBidPromises;
+    });
+  })
+  .then(() => {
+    let createBidPromise = Promise.resolve();
+
+    newBids.forEach((bid) => {
+      const params = {
+        requestBody: bid
+      };
+
+      createBidPromise = createBidPromise.finally(() => {
+        console.log('creating bid for: locationId ' + bid.locationId + ', name ' + bid.name + ', description ' + bid.description);
+        return fgApi.createBid(params).catch((err) => {
+          console.log('creating bid failed for: locationId ' + bid.locationId + ', name ' + bid.name + ', description ' + bid.description);
+          return Promise.reject(err);
+        });
+      });
+    });
+
+    return createBidPromise;
+  })
+  .then(() => {
+    let promises = [];
+    let currentBidsOfLocations = [];
+
+    locationIds.forEach((locationId) => {
+      const params = {
+        requestParams: {locationId: locationId}
+      };
+
+      promises.push(
+        fgApi.listBids(params).then((bids) => {
+          currentBidsOfLocations = currentBidsOfLocations.concat(bids);
+        })
+      );
+    });
+
+    return Promise.all(promises).then(() => {
+      console.log('number of bids: ' + currentBidsOfLocations.length);
+    });
+  })
+  .finally(() => {
+    return fgApi.logout();
+  });
---
+++ node_scripts/fg-api-core.js
@@ -0,0 +1,267 @@
+'use strict';
+
+let fgApiCoreModule = {
+    configure: configure,
+    createBid: createBid,
+    deleteBid: deleteBid,
+    getOffer: getOffer,
+    getOfferDetail: getOfferDetail,
+    getSearch: getSearch,
+    listBids: listBids,
+    listBookedOfferHistories: listBookedOfferHistories,
+    listBookedOffers: listBookedOffers,
+    listBookedOfferVersions: listBookedOfferVersions,
+    listBriefOffers: listBriefOffers,
+    listCalculations: listCalculations,
+    listOffers: listOffers,
+    listOfferSearches: listOfferSearches,
+    login: login,
+    logout: logout
+};
+
+const rp = require('request-promise');
+const Promise = require('bluebird');
+const _ = require('lodash');
+
+let apiPrefix,
+    sessionId
+;
+
+const contextKey = 'BIhYGSk-DlrnijwhGHI-FwoS4etKfqi';
+const defaultOptions = {
+    qs: {
+        contextKey: contextKey
+    },
+    json: true
+};
+
+
+function appendSession() {
+    return sessionId ? (';jsessionid=' + sessionId) : '';
+}
+
+function getMergedFgOptions(options) {
+    return _.merge(_.cloneDeep(defaultOptions), options);
+}
+
+function request(options) {
+    return new Promise((resolve, reject) => {
+        rp(options).then((response) => {
+            resolve(response);
+        }, (err) => {
+            reject(err);
+        });
+    });
+}
+
+
+
+function configure(fgHost) {
+    apiPrefix = fgHost + '/smp/api/';
+}
+
+function createBid(params) {
+    const bid = params.requestBody;
+
+    const options = getMergedFgOptions({
+        body: bid,
+        method: 'POST',
+        uri: setUri('bids2/bids')
+    });
+
+    return request(options);
+}
+
+function deleteBid(params) {
+    const bidKey = params.pathParams.bidKey;
+
+    const options = getMergedFgOptions({
+        method: 'DELETE',
+        uri: setUri('bids2/bids/' + bidKey)
+    });
+
+    return request(options);
+}
+
+function getOffer(params) {
+    const offerKey = params.pathParams.offerKey;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('offer2/offers/' + offerKey)
+    });
+
+    return request(options);
+}
+
+function getOfferDetail(params) {
+    const offerKey = params.pathParams.offerKey;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('offer2/offers_brief/' + offerKey + '/detail')
+    });
+
+    return request(options);
+}
+
+function getSearch(params) {
+    const searchConfigKey = params.pathParams.searchConfigKey;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('offer2/offersearches/' + searchConfigKey)
+    });
+
+    return request(options);
+}
+
+function listBids(params) {
+    const requestParams = params.requestParams;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('bids2/bids'),
+        qs: requestParams
+    });
+
+    return request(options);
+}
+
+function listBookedOfferHistories(params) {
+    const bookedOfferKey = params.pathParams.bookedOfferKey;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('bookedoffers2/' + bookedOfferKey + '/history')
+    });
+
+    return request(options);
+}
+
+function listBookedOffers(params) {
+    const requestParams = params.requestParams;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('bookedoffers2'),
+        qs: requestParams
+    });
+
+    return request(options);
+}
+
+function listBookedOfferVersions(params) {
+    const bookedOfferKey = params.pathParams.bookedOfferKey;
+    const requestParams = params.requestParams;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('bookedoffers2/' + bookedOfferKey + '/versions'),
+        qs: requestParams
+    });
+
+    return request(options);
+}
+
+function listBriefOffers(params) {
+    const requestParams = params.requestParams;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('offer2/offers_brief'),
+        qs: requestParams
+    });
+
+    return request(options);
+}
+
+function listCalculations(params) {
+    const requestParams = params.requestParams;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('offer2/calculations'),
+        qs: requestParams
+    });
+
+    return request(options);
+}
+
+function listOffers(params) {
+    const requestParams = params.requestParams;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('offer2/offers'),
+        qs: requestParams
+    });
+
+    return request(options);
+}
+
+function listOfferSearches(params) {
+    const requestParams = params.requestParams;
+
+    const options = getMergedFgOptions({
+        method: 'GET',
+        uri: setUri('offer2/offersearches'),
+        qs: requestParams
+    });
+
+    return request(options);
+}
+
+function login(params) {
+    const body = params.requestBody;
+
+    const options = getMergedFgOptions({
+        body: body,
+        method: 'POST',
+        uri: setUri('authentication/login/admin')
+    });
+
+    return request(options).then((user) => {
+        sessionId = user.sessionId;
+        return Promise.resolve(user);
+    }, (err) => {
+        return Promise.reject(err);
+    });
+}
+
+function logout() {
+    const options = getMergedFgOptions({
+        method: 'DELETE',
+        uri: setUri('authentication/current'),
+    });
+
+    return request(options).then(() => {
+        sessionId = null;
+        return Promise.resolve();
+    }, (err) => {
+        return Promise.reject(err);
+    });
+}
+
+function setUri(url, ...replacers) {
+    const replacersLength = replacers.length;
+
+    if (replacersLength) {
+        const keys = url.match(/{(\w)+}/g);
+
+        if (keys.length) {
+            _.each(keys, (key, index) => {
+                if (index < replacersLength) {
+                    url = url.replace(key, replacers[index]);
+                }
+            });
+        }
+    }
+
+    return apiPrefix + url + appendSession();
+}
+
+
+
+
+module.exports = fgApiCoreModule;
